ab6c8b2f0826e4eda142cd99a57821a84503f59a
Nitant <nitantgupta08@gmail.com>
2024-10-04 15:37:49 +0530

    theme and colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 /** @type {import('tailwindcss').Config} */
-import {nextui} from "@nextui-org/react";
+import { nextui } from "@nextui-org/react";
 
 module.exports = {
   content: [
@@ -7,19 +7,45 @@ module.exports = {
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
     "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
-    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}"
-
-
-
   ],
   theme: {
+    defaultTheme: "light", // default theme from the themes object
+    defaultExtendTheme: "light",
     extend: {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        primary: {
+          DEFAULT: "#FF6600", // Default primary color
+          50: "rgba(255, 102, 0, 0.1)", // lightest rgba
+          100: "rgba(255, 102, 0, 0.4)", // medium rgba
+          200: "rgba(255, 102, 0, 0.5)", // slightly darker rgba
+          300: "#FAAE62", // FA color
+          400: "#FF6600", // solid orange
+          500: "rgba(255, 102, 0, 1)", // full opacity rgba
+          600: "#AD3113", // dark orange
+          700: "#C8151C", // deeper red-orange
+          800: "rgba(191, 79, 4, 0.8)", // dark rgba
+          900: "#BF4F04", // even darker
+        },
+        secondary: {
+          DEFAULT: "#FAAE62",
+        },
+        accent: {
+          DEFAULT: "#FF6600", // Default accent color
+        },
+        lightGreen: {
+          DEFAULT: "rgba(76, 217, 100, 0.5)",
+        },
+        green: {
+          DEFAULT: "rgba(43, 163, 55, 1)",
+        },
+        yellow: {
+          DEFAULT: "rgba(255, 246, 201, 1)", // Soft yellow/cream color
+        },
       },
     },
   },
   // darkMode: "class",
-  plugins: [nextui()]
+  plugins: [nextui()],
 };
